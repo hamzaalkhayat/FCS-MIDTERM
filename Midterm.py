9eb367870ae7ee41d056c396a8d1c5c3dd517ed5
unknown <hamzaalkhayat96@gmail.com>
2023-11-25 17:18:15 +0200

    starting midterm by adding MainMenu

--- Midterm.py
+++ Midterm.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 24 18:24:42 2023
 
-@author: user
-"""
 
+def mainMenu():
+  choice=-99 # dummy value
+  while choice != '9':
+      
+    print("\nMenu:")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tab")
+    print("6. Sort All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    
+mainMenu()    
+    
