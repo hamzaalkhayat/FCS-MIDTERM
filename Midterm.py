fe4f98f03a0853bd6a9b32bbe22af9b10fa172d8
unknown <hamzaalkhayat96@gmail.com>
2023-11-25 20:03:55 +0200

    adding the first function Open_New_Tab

--- Midterm.py
+++ Midterm.py
@@ -1,6 +1,26 @@
-
+def Open_New_Tab (Order_OpenTabs):
+    Open_Tabs = Order_OpenTabs
+    title = input("Enter the Title of the website: ")
+    url = input("Enter the URL: ")
+   
+    if title == "" or url == "":
+       print("Title OR Url is Empty Please Try Again ") 
+    else:
+     check_title = False   
+     for tab in Open_Tabs:
+       if  tab["title"] == title :
+          check_title = True
+     if check_title == True:
+       print("Title ALready exist!! ") 
+     else:   
+      tab={"title": title, "url": url}
+      Open_Tabs.append(tab)
+      print("Tab "+title+" Opened Successfully.")
+    return Open_Tabs
 
 def mainMenu():
+  Order_OpenTabs=[] 
+    
   choice=-99 # dummy value
   while choice != '9':
       
@@ -17,6 +37,7 @@ def mainMenu():
     
     choice = input("Enter your choice: ")
     
-    
+    if choice == '1':
+      Order_OpenTabs = Open_New_Tab (Order_OpenTabs)
 mainMenu()    
     
